b095fadfa9e85a47a3f6856c2201bb51a46e3668
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2022-04-02 07:42:23 -0400

    Add files via upload
    Create server

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dead Channel</title>
+
+    <link href="/asset/css/index.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+    <section id="gui">
+      <p class="header" id="head-text">Dead Channel</p>
+      <img src="/asset/img/indian-head.jpg" width="300" id="indian-head"/>
+      <img src="/asset/img/static.gif" width="300" id="tv-static"/>
+      <p class="low-header">Dispensotron</p>
+      <p class="btn" id="autoclick">AUTOCLICKER MINUTES</p>
+      <p class="btn" id="scene">SCENE SKIPPER</p>
+      <p class="btn" id="candle">CANDLE WOOD CALCULATOR</p>
+      <input id="code" autocomplete="off"/>
+      <p class="low-header">Owner's Manual</p>
+      <section id="manual">
+        <p>This manual will have details about every thing received from the Dispensotron.</p>
+      </section>
+      <p><a href="https://discord.gg/nBGnytRr8x" target="_blank" class="low-header">Discord</a></p>
+    </section>
+    
+    <script src="/script/animations.js"></script>
+  </body>
+</html>
